e978c94fea7ff60a548b9aafe695d0425e5ce33f
Asadbek <oleg.kadambaev@gmail.com>
2026-07-02 10:00:00 +0500

    feat

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Learnix",
   slug: "learnix",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   scheme: "learnix",
